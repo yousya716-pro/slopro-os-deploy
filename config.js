f9d470dba3a6716b567a4236eb131976dd525095
yousya716-pro <yousya716@gmail.com>
2026-09-14 03:31:58 +0900

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__SLOPRO_PUBLIC_CONFIG__ = Object.freeze({
+  supabaseUrl: 'https://tcrenqxqdjomlqrngvck.supabase.co',
+  publishableKey: 'sb_publishable_xopGCMUQFlRDjHke6CeSIw_q4cOFb9a'
+});
+
