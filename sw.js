9905e7d6522830a6261f829db039595378c3b269
yousya716-pro <yousya716@gmail.com>
2026-09-14 18:30:55 +0900

    Deploy resilient machine API loading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'slopro-public-shell-v6-schematic-map';
+const CACHE_NAME = 'slopro-public-shell-v7-machine-retry';
 const FILES = ['./', './index.html', './styles.css', './config.js', './app.js', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key.startsWith('slopro-public-shell-') && key !== CACHE_NAME).map((key) => caches.delete(key))))));
